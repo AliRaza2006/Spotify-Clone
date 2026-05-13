39cf9835827ffdffa70e1f9b8090a470ea2fa9ed
AliRaza2006 <maliraza2019@gmail.com>
2026-05-13 21:09:21 +0500

    modified server.js

--- Backend/server.js
+++ Backend/server.js
@@ -11,13 +11,9 @@ const albumRoutes=require("./src/routes/albumRoutes")
 app.use("/uploads", express.static("uploads"));
 app.use(cors({
     origin:process.env.CLIENT_URL,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 app.use(express.json())
-app.options("*", cors()); 
 
 app.use("/api/auth",authRoutes)
 app.use("/api/songs",songRoutes)
